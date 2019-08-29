882796d239fb29a9224290c1fb6529be5daea6ed
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2019-08-29 21:47:56 +0300

    Реализация сортировки массива с помощью опорного элемента

---
+++ Array_split.py
@@ -0,0 +1,26 @@
+def ArrayChunk(array):
+#Метод сортирует массив на вокруг опорного элемента
+    if len(a)==0:
+        return None
+    else:
+        while len(array)>0:
+            middle=len(array)//2
+            start=0
+            finish=len(array)-1
+            while len(array)>0:
+                while array[middle]>array[start]:
+                    start+=1
+                while array[middle]<array[finish]:
+                    finish-=1
+                if start==(finish-1) and array[start]>array[finish]:
+                    array[start],array[finish]=array[finish],array[start]
+                    break
+                if start==finish or (start==(finish-1) and array[start]>array[finish]):
+                    #print(array)
+                    return middle
+                array[start], array[finish]=array[finish], array[start]
+            
+ """           
+a=[1,23]
+print(ArrayChunk(a))
+"""
