7467fc5faea280710d180daf077bde9ed1fbb5f5
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2019-09-02 20:59:29 +0300

    Изменил логику работы ф-ции сортировки вокруг опорного элемента

--- Array_split.py
+++ Array_split.py
@@ -8,10 +8,18 @@ def ArrayChunk(array):
             start=0
             finish=len(array)-1
             while len(array)>0:
-                while array[middle]>array[start]:
-                    start+=1
-                while array[middle]<array[finish]:
-                    finish-=1
+                for istart in range(0,len(array)):
+                    if array[middle]>array[start]:
+                        start+=1
+                    else:
+                        istart=0
+                        break
+                for ifinish in range(0,len(array)):
+                    if array[middle]<array[finish]:
+                        finish-=1
+                    else:
+                        ifinish=0
+                        break
                 if start==(finish-1) and array[start]>array[finish]:
                     array[start],array[finish]=array[finish],array[start]
                     break
@@ -19,8 +27,9 @@ def ArrayChunk(array):
                     #print(array)
                     return middle
                 array[start], array[finish]=array[finish], array[start]
+                #print(array)
             
-"""           
-a=[1,23]
+"""          
+a=[7,5,6,4,3,1,2]
 print(ArrayChunk(a))
 """
